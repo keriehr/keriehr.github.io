70c7a6d4acc8a596f356c1bbe70ce91c283bf0c0
Ubuntu <ubuntu@ip-172-31-82-48.ec2.internal>
2020-03-03 20:42:38 +0000

    updated lets get functional

--- projects/let-s-get-functional/index.js
+++ projects/let-s-get-functional/index.js
@@ -21,120 +21,121 @@ var _ = require("lodown-keriehr");
  *    IMPORTANT: Make sure you replace <YOUR_GITHUB_FOLDER with your actual github folder name that is in your workspace.
  */
 
-var maleCount = function(array) {
- return _.filter(array, function(customerObj){
-    return customerObj.gender === "male"; //brings back array property
-}).length; //returning the number
+var maleCount = function(array) { //creating ourfunction with the para array
+ return _.filter(array, function(customerObj){ //using filter to filter thru the array
+    return customerObj.gender === "male"; //brings back array property //checking to see if it is equal to "male"
+}).length; // it is returning the number of how many were equal to male 
 };
 
 
 
 
 
-var femaleCount= function(array) { //output is a number
+var femaleCount= function(array) { //output is a number //creating the function with para array
 
 
 
-return _.reduce(array, function(n, val) {
-    return n + (val.gender === "female");
-}, 0);
+return _.reduce(array, function(n, val) { //using reduce function this time to iterate thru the array
+    return n + (val.gender === "female"); //if the objects gender is equal to female, it will add one to our accumulator.
+}, 0);//a seed to start our accumulator at
+//we return the value that reduce produces
  
 }
 
-//  return _.reduce(array, function(n, customerObj){
-//     return n + (customerObj.gender === "female"); 
 
-// };
 
-var oldestCustomer = function(array){ //output is string
+var oldestCustomer = function(array){ //creating the function with the parameter array
+    //output is string
 
-let highestAge = 0;
-return _.reduce(array, function(prev, curr){
-  if(curr.age > highestAge) {
+let highestAge = 0; //creating a variable of highest age 
+return _.reduce(array, function(prev, curr){ //using reduce to iterate through the array
+  if(curr.age > highestAge) { //if the current object age is great than the highest age...
+  //we change the value of highest age to the current age
     highestAge = curr.age;
-    return curr.name;
+    return curr.name; //then we return the current object's name
   }
-return prev;
-}, 0);
+return prev; //otherwise we return the previous 
+}, 0); //seed to start the accumulator at
  
 };
 
-var youngestCustomer = function(array){ //output is string
+var youngestCustomer = function(array){ //creating the function with the para array
+    //output is string
 
-let youngAge = Infinity;
-return _.reduce(array, function(prev, curr){
-  if(curr.age < youngAge) {
-    youngAge = curr.age;
-    return curr.name;
+let youngAge = Infinity; //creating a variable with the value of infinity
+return _.reduce(array, function(prev, curr){ //using reduce to iterate through the array
+  if(curr.age < youngAge) { //if the current objects age the less than the young age..
+    youngAge = curr.age; // update young age to the current age
+    return curr.name; //then we return the current objects name
   }
-return prev;
-}, 0);
+return prev; //otherwise return previous
+}, 0);//seed to start accumulator at
  
 };
 
 
 
-var averageBalance = function(array){ // output is a number
+var averageBalance = function(array){ //creating a function with the para array
+    // output is a number
  
-//var balance = _.pluck(array, balance)
+
  
- var sum = _.reduce(array, function(total, customer){
-     let amount = customer.balance.slice(1);
-     let string = '';
-     for(let i = 0; i < amount.length; i++){
-         if(amount[i] !== ','){
-             string += amount[i];
+ var sum = _.reduce(array, function(total, customer){ //using reduce to iterate through the array
+     let amount = customer.balance.slice(1); //using slice to cut off the $, and only keep the numbers
+     let string = '';//creating a variable string that is an empty string
+     for(let i = 0; i < amount.length; i++){ //using for loop to loop through the amount
+         if(amount[i] !== ','){ //if it is not a comma, then we are going to add
+             string += amount[i]; //here we add the number to the string
          }
      }
      
-     string = parseFloat(string);
+     string = parseFloat(string); //this lets us use the numbers in the string as numbers
      
-     return total += string;
-}, 0);
+     return total += string; //here we return the total of all the numbers added together
+}, 0);//seed for accum. to start at
  
- return sum / array.length;
+ return sum / array.length; //then we return the sum divded by the length of the array  to get an average
  
-}; //arr.reduce((a,b) => a + b, 0) / arr.length
+}; 
 
 
 
-
-
-var firstLetterCount = function(array, letter){ //returns a number
+var firstLetterCount = function(array, letter){ //creating a function that takes the para, array and letter
+    //returns a number
     
-    // let strName = ""
-   // string.charAt(0)
    
    
-    return _.reduce(array, function(n, object) {
+    return _.reduce(array, function(n, object) { //using reduce to iterate through the array
         
+        //if the letter is equal to the objects names' first letter, whether it upper or lower case, 
+        //then it will add it to our accumulator "n"
+        // and it will return that final number 
          return n + (letter === object['name'].charAt(0).toUpperCase() || letter === object['name'].charAt(0).toLowerCase() );
-}, 0);
+}, 0); //seed to start accum. at
         
-        
-//     return n + (val.gender === "female");
-// }, 0);
-    
+  
 } ;
 
-var friendFirstLetterCount = function(array, customer, letter){ //output is number
+var friendFirstLetterCount = function(array, customer, letter){ //creating a function that takes the para, array, customer, and letter
+    //output is number
     
-    let friendsarr = [];
+    let friendsarr = []; //creating an empty array
     
- for(let i = 0; i < array.length ; i++){                           
+ for(let i = 0; i < array.length ; i++){  //using for loop to loop through the array                
  
                                                                      
-     if (array[i].name === customer){
-         friendsarr = array[i].friends;
+     if (array[i].name === customer){ // if the name is equal to the give customer..
+         friendsarr = array[i].friends; //then add the friends to our variable freindsarr
      }
      
  }
-        
-        return _.reduce(friendsarr, function(acc, nameObject){
-            if (nameObject.name.charAt(0).toUpperCase() === letter.toUpperCase()){
-                return acc += 1;
-            } return acc
-        }, 0);
+        //returning the product of this function which is the total amount of friends from given customer, thats names starts with given letter
+        return _.reduce(friendsarr, function(acc, nameObject){ //using reduce to iterate thru the array
+            if (nameObject.name.charAt(0).toUpperCase() === letter.toUpperCase()){ 
+                //if the name in the friendsarr starts with the given letter..
+                return acc += 1; //then add 1 to accum.
+            } return acc //return the accum. amount
+        }, 0); //seed to start accum. at
         
   
 };
@@ -143,38 +144,36 @@ var friendFirstLetterCount = function(array, customer, letter){ //output is numb
 
 
 
-var friendsCount = function(array, name){  //returns array
+var friendsCount = function(array, name){  //creating a function with the para, array and name
+    //returns array
     
     //return the names of customers in an array that have the given name in their friends list
     
     
-//   return _.reduce(array, function(acc, currentObj) {
-    //if it has the given name in the customers friends list.. push it to an array.. then return that array
     
-    
-      let friendsarr = [];
+      let friendsarr = []; //created 2 different arrays
      let allfriends = []
     
- for(let i = 0; i < array.length ; i++){                           
+ for(let i = 0; i < array.length ; i++){ //using for loop to loop thru given array                 
  
                                                                      
-     if (array[i].name){
-         friendsarr = array[i].friends;
+     if (array[i].name){ //if the array a name..
+         friendsarr = array[i].friends; // then add the friends array to friendsarr
          
      }
      
    
         
         
-         _.reduce(friendsarr, function(acc, nameObject){
-            if (nameObject.name === name){
-                 allfriends.push(array[i].name)
+         _.reduce(friendsarr, function(acc, nameObject){ //using reduce to iterate thru the new friendsarr
+            if (nameObject.name === name){ //if the friendsarr contains the given name..
+                 allfriends.push(array[i].name) // the add that customers name to the allfriends array
             }
         }, 0);}
     
     
     
-    return allfriends
+    return allfriends //returning the final array
     
     
     
@@ -182,110 +181,68 @@ var friendsCount = function(array, name){  //returns array
     
 };
 
-var topThreeTags = function(array){ //output array 
+var topThreeTags = function(array){  //creating function with the para, array
+    //output array 
     
 
-    let tag = [];
-    for (let i = 0; i < array.length; i++) {
-        tag.push(array[i].tags);
-       
-    } for (let i = 0; i < tag.length; i++){
-        tag.concat(tag[i])
-    } console.log(tag)
+    let tag = []; //creating an array
+    for (let i = 0; i < array.length; i++) { //using for loop to loop thru given array
+        tag.push(array[i].tags); //adding the tags array into the new tag array
+    }
+    var merged = [].concat.apply([], tag); //then turning all those little arrays into one big single array
+   
     
-    let count = tag.reduce((arr, index) => {
-        arr[index] = (arr[index] || 0) + 1;
-        return arr;
+    let count = merged.reduce((arr, index) => {  //using reduce to iterate through the merged array
+        arr[index] = (arr[index] || 0) + 1; //keeping count of each tag/element
+        return arr; //returning the accumulator
     }, {});
-    console.log(count)
+    
     let maxCount = Math.max(...Object.values(count));
-     //returns the largest of numbers    //this brings back the values
-     console.log(maxCount)
+     //math.max returns the largest of numbers    // obj.values, brings back the values of our count object
+     
     let mostFrequent = Object.keys(count).filter(k => count[k] === maxCount);
                 //this is the tags names    //filtering thru the keys,  equaling to max count
     
-    // if (mostFrequent.length >= 3) {
-    //     return mostFrequent;
-    // } else {return mostFrequent;
+    if (mostFrequent.length >= 3) { //if the length of most frequent is greater than or equal to 3..
+        return mostFrequent; //then return mostfrequent  // this gives us the top 3 tags
+    } else {return mostFrequent; //otherwise returning most frequent 
 
-    // }  
+    }  
     
-    
-    
-    
-    
-    
-    
-    
-    
-//     let tag = []
-//     for (let i = 0; i < array.length; i++){
-//         tag.concat(array[i].tags);
-//     }
-    
-// let wordCount = _.reduce(tag, function(acc, currWord){
 
-  
-// if (acc[currWord]){ 
-//   acc[currWord] += 1;   //here we increment
-// return acc;
-// }
-// acc[currWord] = 1 // making key value pair if it didnt exist
-
-// return acc;
-
-//   }, {})
-
-
-// var nestedArray = [];
-
-// _.each(wordCount, function(count, word, object){
-//   nestedArray.push([word, count])
-// })
-
-
-// nestedArray.sort(function(a, b){
-//   return b[1] - a[1];
-// })
-
-
-// let resultArr = [];
-
-// for(let i = 0; i < 3; i++){
-//   resultArr.push(nestedArray[i][0])
-// }
-
-// return resultArr
-
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
     
     
 };
  
 
-var genderCount = function(array){
+var genderCount = function(array){ //creating function that taske the parameter, array
+    
+    //creating variables i will later use
+    var male
+    var female
+    var nonbin
     
     
+     male = _.reduce(array, function(n, val) { //using reduce to return the amount of males
+    return n + (val.gender === "male");
+}, 0);
     
     
-    
-    
-// return _.reduce(array, function(n, val) {
-//     return n + (val.gender === "female");
-// }, 0);
+ female = _.reduce(array, function(n, val) { //using reduce to return the amount of females
+    return n + (val.gender === "female");
+}, 0);
  
-// }
-    
+ nonbin =  _.reduce(array, function(n, val) { //using reduce to return thr amount of others
+    return n + (val.gender === "non-binary");
+}, 0);
+
+var key = "non-binary" //creating key called non binary
+    var resultObj = {} //creating object to push values into
+ resultObj.male = male // adding male count into object as a key value pair
+resultObj.female = female    // adding female count into object as a key value pair
+resultObj[key] = nonbin //adding non binary amount value into object
+
+return resultObj //returning the final object with all genders and amount of each  as key value pairs.
     
     
 };
